665cafb3076ee5565ecede9af1fd0d40f5b813aa
Jimmy Siquiej <jr.scuc@gmail.com>
2015-07-20 16:31:58 -0600

    Tarea No. 1 Laboratorio de Compiladores 1.

---
+++ HolaMundoGit/src/holamundogit/HolaMundoGit.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundogit;
+
+/**
+ *
+ * @author Jimmy
+ */
+public class HolaMundoGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("**************************************************************");
+        System.out.println("######### ORGANIZACION DE LENGUAJES Y COMPILADORES 1 #########");
+        System.out.println("**************************************************************");
+        System.out.println("");
+        System.out.println("Hola Mundo!");
+        System.out.println("");
+        System.out.println("Datos del estudiante: ");
+        System.out.println("Nombre: Jimmy Ronald Siquiej Cuc");
+        System.out.println("Carnet: 200611655");
+        System.out.println("e-mail: 200611655@ingenieria.usac.edu.gt");
+        
+    }
+    
+}
